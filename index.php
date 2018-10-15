5f1bd6db5d65066fa96146ac6215785ec733b0ba
BuildTools <clement.ababou@outlook.com>
2018-10-16 01:51:38 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Camagru</title>
+    <link rel="stylesheet" href="style/main.css">
+    <link rel="stylesheet" href="style/index.css">
+    <link rel="stylesheet" href="style/material/base_button.css">
+</head>
+<body>
+    <div style="width: 100%; text-align: center; margin-top: 150px;">
+        <span class="material_base-button">
+            <span class="hover-anim"></span>
+            <span class="text">Okay</span>
+        </span>
+    </div>
+</body>
+</html>
